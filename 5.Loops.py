749aa85dfe77e9dd5deb236ece58305e3efd0e89
Shagun <shagunagarwal75@gmail.com>
2023-03-14 10:53:54 +0530

    First Commit

---
+++ 5.Loops.py
@@ -0,0 +1,31 @@
+"""
+if, While and for loop """
+
+#if statement
+
+"""
+a = 200
+b = 33
+if b > a:
+  print("b is greater than a")
+elif a == b:
+  print("a and b are equal")
+else:
+  print("a is greater than b")
+"""
+
+#while loop
+
+"""
+i = 0
+while i < 6:
+  i += 1
+  if i == 3:
+    continue
+  print(i)
+"""
+
+#for loop
+fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+  print(x)
